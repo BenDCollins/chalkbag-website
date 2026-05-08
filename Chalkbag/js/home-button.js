f54597836b7d7efbffc86cc10a3a1d91fd8e0e2a
Ben Collins <bakingben@gmail.com>
2026-05-08 01:25:05 -0400

    Add files via upload
    Initial commit - Chalkbag website

---
+++ Chalkbag/js/home-button.js
@@ -0,0 +1,13 @@
+/* Ben Collins */
+"use strict";
+
+function newsletter(){
+	window.location.href = "newsletter.html"
+}
+
+document.querySelector("#join").addEventListener("click", newsletter);
+
+
+
+
+
